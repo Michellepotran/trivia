6f7a8e8656adace7ea393734ff22e0c5e82724c6
Michellepotran <113163108+Michellepotran@users.noreply.github.com>
2024-04-21 18:39:47 -0600

    created trivia app

---
+++ app/data.js
@@ -0,0 +1,36 @@
+export const quiz = {
+    totalQuestions: 5,
+    questions: [
+      {
+        id: 1,
+        question: 'Red Vines is a brand of what type of candy?',
+        answers: ['Licorice', 'Lollipop', 'Chocolate', 'Bubblegum'],
+        correctAnswer: 'Licorice',
+      },
+      {
+        id: 2,
+        question: 'Which of the following is not an Ivy League University?',
+        answers: ['Harvard', 'Stanford', 'University of Pennsylvania', 'Princeton'],
+        correctAnswer: 'Stanford',
+      },
+      {
+        id: 3,
+        question: 'What does a milliner make and sell?',
+        answers: ['Shirts', 'Belts', 'Shoes', 'Hats'],
+        correctAnswer: 'Hats',
+      },
+      {
+        id: 4,
+        question: 'The Canadian $1 coin is colloquially known as a what?',
+        answers: ['Boolie', 'Loonie', 'Foolie', 'Moodie'],
+        correctAnswer: 'Loonie',
+      },
+      {
+        id: 5,
+        question: 'What character was once considered to be the 27th letter of the alphabet?',
+        answers: ['Ampersand', 'Interrobang', 'Tilde', 'Pilcrow'],
+        correctAnswer: 'Ampersand',
+      },
+    ],
+  };
+  
